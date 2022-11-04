688b04893fc8594e9e677c29b7589d7611f20fcb
Unia909 <ussunk918@gmail.com>
2022-11-04 12:25:33 +0300

    fixes in win32.platform

--- win32/platform.lua
+++ win32/platform.lua
@@ -2,7 +2,6 @@ local ffi = require "ffi"
 require "win32.winntdef"
 local ntstr = require "win32.string"
 local winreg = require "win32.winreg"
-local os = require "win32.os"
 
 ffi.cdef [[
     int GetComputerNameW(const wchar_t *lpBuffer, DWORD *nSize);
@@ -19,35 +18,38 @@ local releases = {
     ["10.0"] = "10Professional"
 }
 
+local function node()
+    local buf = ffi.new("wchar_t[15]") -- max computer name size is 15 o_O
+    local size = ffi.cast("DWORD*", ffi.C.malloc(4))
+    ffi.C.GetComputerNameW(buf, size)
+    local nodesize = size[0]
+    ffi.C.free(size)
+    return ntstr.convtostr(buf, nodesize)
+end
+
+local reg = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, "SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion")
+local curver = winreg.QueryValueEx(reg, "CurrentVersion")
+local ver = curver.."."..winreg.QueryValueEx(reg, "CurrentBuildNumber")
+winreg.CloseKey(reg)
+local rel = releases[curver]
+local processor = require("win32.os").getenv("PROCESSOR_IDENTIFIER")
+
 return {
     machine = function()
-        return st.machine
+        return jit.arch
     end,
-    node = function()
-        return st.node
-    end,
+    node = node,
     release = function()
-        return st.release
+        return rel
     end,
     uname = function()
-        local reg = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, "SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion")
-        local curver = winreg.QueryValueEx(reg, "CurrentVersion")
-
-        local buf = ffi.new("wchar_t[15]") -- max computer name size is 15 o_O
-        local size = ffi.cast("DWORD*", ffi.C.malloc(4))
-        ffi.C.GetComputerNameW(buf, size)
-        local nodesize = size[0]
-        ffi.C.free(size)
-
-        local out = {
+        return {
             system = "Windows",
-            node = ntstr.convtostr(buf, nodesize),
-            release = releases[curver],
-            version = curver.."."..winreg.QueryValueEx(reg, "CurrentBuildNumber"),
+            node = node(),
+            release = rel,
+            version = ver,
             machine = jit.arch,
-            processor = os.getenv("PROCESSOR_IDENTIFIER")
+            processor = processor
         }
-        winreg.CloseKey(reg)
-        return out
     end
 }
